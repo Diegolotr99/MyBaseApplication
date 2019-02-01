0f6f778a28a2ffe36165d0438f458aa3a1ca3883
Diego Gutierrez <diegolotr99@gmail.com>
2019-02-01 00:35:36 -0600

    snackbars

--- app/src/main/java/com/diegogutierrez/mybaseapplication/modules/main/MainActivity.kt
+++ app/src/main/java/com/diegogutierrez/mybaseapplication/modules/main/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.diegogutierrez.mybaseapplication.modules.main
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.DataBindingUtil.setContentView
 import androidx.lifecycle.ViewModelProviders
 import com.diegogutierrez.mybaseapplication.R
 import com.diegogutierrez.mybaseapplication.modules.baseComponents.BaseActivity
 import com.diegogutierrez.mybaseapplication.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
-class MainActivity: BaseActivity<MainViewModel, ActivityMainBinding>(), MainView {
+class MainActivity: BaseActivity<MainViewModel, ActivityMainBinding>(), MainView, View.OnClickListener {
+
 
 
     override val viewModel: MainViewModel
@@ -23,5 +25,10 @@ class MainActivity: BaseActivity<MainViewModel, ActivityMainBinding>(), MainView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onClick(p0: View?) {
+        val snackBar = Snackbar.make(getRootView(), "Success", Snackbar.LENGTH_LONG)
+        snackBar.show()
     }
 }
--- app/src/main/java/com/diegogutierrez/mybaseapplication/modules/main/MainViewModel.kt
+++ app/src/main/java/com/diegogutierrez/mybaseapplication/modules/main/MainViewModel.kt
@@ -4,10 +4,10 @@ import com.diegogutierrez.mybaseapplication.modules.baseComponents.BaseViewModel
 
 class MainViewModel: BaseViewModel<MainView>() {
     override fun onStart() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun onStop() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 }
